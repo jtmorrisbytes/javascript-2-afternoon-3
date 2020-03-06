aa13dac88e4f725f999b3833508baccb2735ad55
Jordan Taylor Morris <jordan@jordans-mbp.attlocal.net>
2020-03-06 12:53:03 -0600

    finished problems 6 & 7, corrected uniq

--- practice.js
+++ practice.js
@@ -27,20 +27,18 @@
   Then invoke the callback function, passing in the first element in the array as it's argument.
 */
 
-// Code Here 
+// Code Here
 function first(array, callback) {
   callback(array[0]);
 }
 // Do not edit the code below.
-var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+var names = ["Tyler", "Cahlan", "Ryan", "Colt", "Tyler", "Blaine", "Cahlan"];
 
-first(names, function(firstName){
-  console.log('The first name in names is ' + firstName);
+first(names, function(firstName) {
+  console.log("The first name in names is " + firstName);
   return firstName;
 });
 // Do not edit the code above.
-
-
 
 ////////// PROBLEM 2 //////////
 
@@ -50,17 +48,15 @@ first(names, function(firstName){
 */
 
 //Code Here
-function last(array,callback) {
-  callback(array[array.length-1]);
+function last(array, callback) {
+  callback(array[array.length - 1]);
 }
 // Do not edit the code below.
-last(names, function(lastName){
-  console.log('The last name in names is ' + lastName);
+last(names, function(lastName) {
+  console.log("The last name in names is " + lastName);
   return lastName;
 });
 // Do not edit the code above.
-
-
 
 ////////// PROBLEM 3 //////////
 
@@ -70,16 +66,14 @@ last(names, function(lastName){
 */
 
 //Code Here
-function multiply(num1,num2,callback) {
+function multiply(num1, num2, callback) {
   callback(num1 * num2);
 }
 // Do not edit the code below.
-multiply(4, 3, function(answer){
-  console.log('The answer is ' + answer); //should console.log 12
+multiply(4, 3, function(answer) {
+  console.log("The answer is " + answer); //should console.log 12
 });
 // Do not edit the code above.
-
-
 
 ////////// PROBLEM 4 //////////
 
@@ -90,26 +84,23 @@ multiply(4, 3, function(answer){
   If the name does not exist, invoke the callback with false as the argument.
 */
 
-//Code Here 
+//Code Here
 function contains(array, name, callback) {
-  if(array.includes(name)) {
-    callback(true)
-  }
-  else{
+  if (array.includes(name)) {
+    callback(true);
+  } else {
     callback(false);
   }
 }
 // Do not edit the code below.
-contains(names, 'Colt', function(result){
-  if(result === true){
-    console.log('Colt is in the array');
+contains(names, "Colt", function(result) {
+  if (result === true) {
+    console.log("Colt is in the array");
   } else {
-    console.log('Colt is not in the array');
+    console.log("Colt is not in the array");
   }
 });
 // Do not edit the code above.
-
-
 
 ////////// PROBLEM 5 //////////
 
@@ -119,30 +110,28 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
-function uniq(array,callback){
-  array.sort();
-
-  for (let i = array.length - 1; i >=0; i--) {
-    if(array.indexOf(array[i]) >=0) {
-      if(i !== array.lastIndexOf(array[i])) {
-        while(i !== array.lastIndexOf(array[i])) {
-          array.splice(array.lastIndexOf(i))
+function uniq(arr, callback) {
+  for (let start = arr.length - 1; start >= 0; start--) {
+    // console.log("START",start)
+    for (let searchIndex = start - 1; searchIndex >= 0; searchIndex--) {
+      if (arr[start] === arr[searchIndex]) {
+        arr.splice(start, 1);
+        if (start != arr.length - 1) {
+          start++;
         }
       }
-      
-      
     }
-    
   }
-  callback(array);
+  callback(arr);
 }
 // Do not edit the code below.
-uniq(names, function(uniqArr){
-  console.log('The new names array with all the duplicate items removed is ', uniqArr);
+uniq(names, function(uniqArr) {
+  console.log(
+    "The new names array with all the duplicate items removed is ",
+    uniqArr
+  );
 });
 // Do not edit the code above.
-
-
 
 ////////// PROBLEM 6 //////////
 
@@ -151,48 +140,62 @@ uniq(names, function(uniqArr){
   For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
 */
 
-//Code Here 
-
+//Code Here
+function each(arr, callback) {
+  arr.forEach((element, index) => callback(element, index));
+}
 // Do not edit the code below.
-each(names, function(item, indice){
-  console.log('The item in the ' + indice + ' position is ' + item)
+each(names, function(item, indice) {
+  console.log("The item in the " + indice + " position is " + item);
 });
 // Do not edit the code above.
-
-
 
 ////////// PROBLEM 7 //////////
 
 /*
-  Write a function called getUserById that takes in three parameters: an array of objects (users), an id and a callback, and searches for the user with a matching id.
+  Write a function called getUserById that takes in three parameters: an array of objects (users),
+   an id and a callback, and searches for the user with a matching id.
   When the correct user object is found, invoke the callback with the user object as an argument.
 */
 
 // Code here
-
+function getUserById(users, id, cb) {
+  users.forEach(user => {
+    if (user.id === id) {
+      cb(user);
+    }
+  });
+}
 // Do not edit the code below.
 var users = [
   {
-    id: '12d',
-    email: 'tyler@gmail.com',
-    name: 'Tyler',
-    address: '167 East 500 North'
+    id: "12d",
+    email: "tyler@gmail.com",
+    name: "Tyler",
+    address: "167 East 500 North"
   },
   {
-    id: '15a',
-    email: 'cahlan@gmail.com',
-    name: 'Cahlan',
-    address: '135 East 320 North'
+    id: "15a",
+    email: "cahlan@gmail.com",
+    name: "Cahlan",
+    address: "135 East 320 North"
   },
   {
-    id: '16t',
-    email: 'ryan@gmail.com',
-    name: 'Ryan',
-    address: '192 East 32 North'
-  },
+    id: "16t",
+    email: "ryan@gmail.com",
+    name: "Ryan",
+    address: "192 East 32 North"
+  }
 ];
 
-getUserById(users, '16t', function(user){
-  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
+getUserById(users, "16t", function(user) {
+  console.log(
+    "The user with the id 16t has the email of " +
+      user.email +
+      " the name of " +
+      user.name +
+      " and the address of " +
+      user.address
+  );
 });
 // Do not edit the code above.
